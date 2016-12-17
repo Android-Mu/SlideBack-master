7b6addce9a0f3d860cf7c78f204b35574de980df
Android-Mu <yun12712@163.com>
2016-12-17 17:57:57 +0800

    Initial commit

---
+++ sample/src/main/java/com/megabox/android/slide/MainActivity.java
@@ -0,0 +1,29 @@
+package com.megabox.android.slide;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.megabox.android.slide.sample.R;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // 首页禁用滑动返回
+        setSlideable(false);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showBackButton(false);
+
+        findViewById(R.id.btn_click_me).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
